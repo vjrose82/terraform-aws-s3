b1350d64ae1b1d60a56aa54f410e851d55a054b5
ricky <balkaran@cloudprodigy.ca>
2022-07-11 09:11:23 -0400

    updated aws provider version

--- main.tf
+++ main.tf
@@ -1,37 +1,53 @@
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "${var.account_id}-${var.bucket_name}"
-  acl    = "private"
-  tags   = local.common_tags
+resource "aws_s3_bucket" "s3_bucket" { #tfsec:ignore:AWS002 tfsec:ignore:AWS017 tfsec:ignore:AWS077
+  bucket = var.bucket_name
 
-  logging {
-    target_bucket = var.logging_bucket_name
-    target_prefix = "${var.bucket_name}/"
+  tags = local.common_tags
+}
+
+resource "aws_s3_bucket_acl" "this" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_logging" "this" {
+  bucket        = aws_s3_bucket.s3_bucket.id
+  target_bucket = var.logging_bucket_name
+  target_prefix = "${var.bucket_name}/"
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  count  = var.versioning == "yes" ? 1 : 0
+  bucket = aws_s3_bucket.s3_bucket.id
+  versioning_configuration {
+    status = "Enabled"
   }
-  versioning {
-    enabled = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
+}
+
+resource "aws_s3_bucket_cors_configuration" "this" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET"]
     allowed_origins = ["*"]
     max_age_seconds = "3000"
   }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 
 }
-
 resource "aws_s3_bucket_public_access_block" "s3_bucket_acl" {
   bucket = aws_s3_bucket.s3_bucket.id
 
-  block_public_acls       = true
-  block_public_policy     = true
+  block_public_acls       = var.block_public_acls
+  block_public_policy     = var.block_public_policy
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
-
--- variables.tf
+++ variables.tf
@@ -1,22 +1,36 @@
-variable "environment" {
-  description = "The environment"
-  type        = string
-}
-
 variable "bucket_name" {
-  description = "Bucket name to be created"
-  type        = string
-}
-
-variable "account_id" {
-  description = "AWS Account ID"
+  description = "The name of the S3 private bucket."
   type        = string
 }
 
 variable "logging_bucket_name" {
-  description = "Bucket to store S3 access logs"
+  description = "The name of S3 logging bucket name"
   type        = string
 }
+
+variable "environment" {
+  description = "Provide appropriate environment name"
+  type        = string
+}
+
+variable "versioning" {
+  description = "Enable or Disable versioning"
+  default     = "no"
+  type        = string
+}
+
+variable "block_public_acls" {
+  description = "Set this variable to false if public access is requested."
+  type        = bool
+  default     = true
+}
+
+variable "block_public_policy" {
+  description = "Set this variable to false is public access is requested."
+  type        = bool
+  default     = true
+}
+
 
 locals {
   common_tags = {
